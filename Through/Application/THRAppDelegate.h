91bf673107f3e373f63f12ad5d0fe5dc15041f19
Renzo Crisóstomo <ruenzuo@gmail.com>
2014-05-30 12:12:02 +0200

    Initial commit

---
+++ Through/Application/THRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  THRAppDelegate.h
+//  Through
+//
+//  Created by Renzo Crisóstomo on 30/05/14.
+//  Copyright (c) 2014 Renzo Crisóstomo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface THRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Through/Supporting/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Through
+//
+//  Created by Renzo Crisóstomo on 30/05/14.
+//  Copyright (c) 2014 Renzo Crisóstomo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "THRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([THRAppDelegate class]));
+    }
+}
